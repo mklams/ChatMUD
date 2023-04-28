5238bcb50255a0cf0a1294113dc71b5cd79753a8
Mark Klamik <mklams@gmail.com>
2023-04-27 22:11:15 -0500

    Add redis database

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const http = require('http');
 const { Server } = require("socket.io");
 const Game = require('./game/game');
+const { createClient } = require('redis');
 
 const app = express();
 app.use(cors());
@@ -20,3 +21,17 @@ app.use('/images', express.static('images'));
 
 // start the game
 const game = new Game(io);
+
+
+// TODO: move database code to own file
+// TODO: save/load users to database
+async function setupDatabase(){
+  const client = createClient();
+  client.on('error', err => console.log('Redis Client Error', err));
+  await client.connect();
+  await client.set('key', 'valueForKey');
+  const value = await client.get('key');
+  console.log(value);
+}
+
+setupDatabase()
